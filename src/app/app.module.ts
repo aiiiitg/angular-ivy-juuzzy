6e46a5bf3bcf84e73b82ae4dcea06f9d3477efe3
aiiiitg <aiiiit@gmail.com>
2022-04-25 10:54:11 +0200

    MatIconModule code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { MatIconModule } from '@angular/material';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { ItemComponent } from './item/item.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, MatIconModule ],
+  exports:      [ MatIconModule ],
   declarations: [ AppComponent, ItemComponent ],
   bootstrap:    [ AppComponent ]
 })
