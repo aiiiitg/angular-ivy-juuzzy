8c3c122f9542f618534770570c83d46920293ddf
aiiiitg <aiiiit@gmail.com>
2022-04-25 18:24:30 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { AppComponent } from './app.component'; // main todo app with list and d
 import { AppRoutingModule } from './app-routing.module'; // routing to map routes between list and detail views
 import { ItemComponent } from './item/item.component'; // todo item for list view
 import { ViewListComponent } from './view/view-list.component'; // list view
+import { ViewDetailComponent } from './view/view-detail.component'; // detail view
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, MatIconModule, AppRoutingModule ],
-  declarations: [ AppComponent, ItemComponent, ViewListComponent ],
+  declarations: [ AppComponent, ItemComponent, ViewListComponent, ViewDetailComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
