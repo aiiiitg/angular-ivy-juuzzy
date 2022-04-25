bc14a531b9aaf080f3d98ec06e16195cc62cec58
aiiiitg <aiiiit@gmail.com>
2022-04-25 10:37:05 +0200

    Add Angular Material for icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { ItemComponent } from './item/item.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, MatIconModule ],
   declarations: [ AppComponent, ItemComponent ],
   bootstrap:    [ AppComponent ]
 })
