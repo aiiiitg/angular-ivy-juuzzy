85d661453e8e5e2fc60ecd9309ca79bc95da5b1a
aiiiitg <aiiiit@gmail.com>
2022-04-25 15:59:18 +0200

    1 file modified

--- src/app/view/view-list.component.ts
+++ src/app/view/view-list.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: './view/view-list.component.html',
-  styleUrls: [ './view/view-list.component.css' ]
+  template: './view-list.component.html',
+  styleUrls: [ './view-list.component.css' ]
 })
 export class ViewListComponent implements OnInit {
   constructor() {}
