57dc4732bf74805313ade89a205558f330c658e1
aiiiitg <aiiiit@gmail.com>
2022-04-26 19:09:47 +0200

    2 files modified

--- src/app/view/view-list.component.ts
+++ src/app/view/view-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Item } from '../item';
 import { TodoService } from '../todo.service';
@@ -9,9 +9,10 @@ import { TodoService } from '../todo.service';
   styleUrls: [ '../app.component.css' ]
 })
 export class ViewListComponent implements OnInit {
+  add: boolean = false;
   items: Item[] = [];
 
-  constructor(private route: ActivatedRoute, public todoService: TodoService) {}
+  constructor(private route: ActivatedRoute, private eRef: ElementRef, public todoService: TodoService) {}
   ngOnInit() {
     this.getItems();
   }
@@ -39,5 +40,15 @@ export class ViewListComponent implements OnInit {
   
   remove(item: Item) {
     return this.todoService.remove(item);
+  }
+
+
+  @HostListener('document:click', ['$event'])
+  clickout(event) {
+    // save editing on click outside the editable component
+    if(this.view === 'list' && this.editable && !this.eRef.nativeElement.contains(event.target) && event.target.id!=("edit"+this.item.id)) {
+      console.log("Save on click-outside item [was editing id="+ this.item.id /*+" "+ this.eRef.nativeElement*/ +" hit id="+ event.target.id +"]")
+      this.saveItem((<HTMLInputElement>document.getElementById("edited"+this.item.id)).value);
+    }
   }
 }
